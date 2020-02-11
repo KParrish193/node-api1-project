19e2d95fc156d062695ab03bc23f4e2844939945
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-02-11 13:59:34 -0700

    early server set up

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // implement your API here
+const express = require('express'); //import express
+
+const server = express() //declare server
+
+const Users = require('.data/db.js')
+
+server.use(express.jsaon());
+
+//initial get
+server.get('/', (req, res) => {
+    res.jason({server: "running"})
+})
+
+//get users
+server.get('/api/users', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err =>{
+            console.log(err)
+            res.status(500).json({ errorMessage: "The user info could not be retrieved"})
+        })
+})
+//
+
+
+
+
+
+const port = 5000; //declare our port
+
+server.listen(port, () => console.log(`/n API listening on port ${port} \n`)); //tell server to listen on our port
